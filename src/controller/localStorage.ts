7c3ab183df43a854d826195f5883f77ced42fe0e
Parfionov Anton <toystix1992@gmail.com>
2022-01-02 03:46:37 +0300

    feat: add App

--- src/controller/localStorage.ts
+++
@@ -1,9 +0,0 @@
-export function setPropertyToStorage<T>(key: string, value: T): void {
-  window.localStorage.setItem(key, JSON.stringify(value))
-}
-export function getPropertyFromStorage<T>(key: string): T {
-  return JSON.parse(window.localStorage.getItem(key))
-}
-export function removePropertyFromStorage(key: string): void {
-  window.localStorage.removeItem(key)
-}
